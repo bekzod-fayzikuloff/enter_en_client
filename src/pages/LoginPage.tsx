44a24df723fdbf96c6918ea4f378b3268074e181
fayzikuloff.b <fayzikuloff.b@gmail.com>
2023-07-21 00:54:14 +0500

    simple change ui

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,47 +1,124 @@
 import {useNavigate} from "react-router-dom";
 import {SubmitHandler, useForm} from "react-hook-form";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import { AuthContext } from '../context/AuthContext';
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
+import {object, string, TypeOf} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Grid, Link,
+  TextField,
+  Typography
+} from "@mui/material";
 
-interface ILoginFormInput {
-  username: string;
-  password: string;
-}
 
-const LoginForm = () => {
-  const navigate = useNavigate()
-  const {register, handleSubmit} = useForm<ILoginFormInput>();
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const { loginUser } = useContext(AuthContext);
-  const onSubmit: SubmitHandler<ILoginFormInput> = async (data) => {
-    loginUser(
-      data.username,
-      data.password,
-      () => {
-        navigate('/');
-      },
-      () => {
-        alert("This user no exist")
-      }
-    );
-  }
+const loginSchema = object({
+  username: string().nonempty('Username is required'),
+  password: string()
+    .nonempty('Password is required')
+    .min(8, 'Password must be more than 8 characters')
+    .max(32, 'Password must be less than 32 characters'),
+})
 
-  return (
-    <div className={"auth__form"}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input placeholder={"Username..."} {...register("username")} /> <br/>
-
-        <input type={"password"} placeholder={"Password..."} {...register("password")} /> <br/>
-
-        <input type="submit"/>
-      </form>
-    </div>
-  );
-}
+type LoginInput = TypeOf<typeof loginSchema>;
 
 export const LoginPage = () => {
+
+  const {
+    register,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+    handleSubmit,
+  } = useForm<LoginInput>({
+    resolver: zodResolver(loginSchema),
+  });
+
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+  }, [isSubmitSuccessful, reset]);
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const {loginUser} = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const onSubmitHandler: SubmitHandler<LoginInput> = (values) => {
+    loginUser(
+      values.username,
+      values.password,
+      () => {
+        navigate("/")
+      },
+      () => {
+        alert("Error")
+      }
+    )
+  };
+
   return (
-    <LoginForm/>
-  )
+    <Container component="main" maxWidth="xs">
+      <CssBaseline/>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{m: 5, bgcolor: 'secondary.main'}}>
+          <LockOutlinedIcon/>
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit(onSubmitHandler)} noValidate sx={{mt: 1}}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Username"
+            error={!!errors['username']}
+            helperText={errors['username'] ? errors['username'].message : ''}
+            {...register('username')}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Password"
+            type="password"
+            error={!!errors['password']}
+            helperText={errors['password'] ? errors['password'].message : ''}
+            {...register('password')}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{mt: 3, mb: 2}}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link onClick={() => navigate("/register")}  variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,48 +1,140 @@
+import {useNavigate} from "react-router-dom";
 import {SubmitHandler, useForm} from "react-hook-form";
+import {useEffect} from "react";
+import {object, string, TypeOf} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Grid, Link,
+  TextField,
+  Typography
+} from "@mui/material";
 import {sendData} from "../utils";
 import {registerUrl} from "../constants/endpoints";
-import {useNavigate} from "react-router-dom";
 
 
-interface IRegisterFormInput {
-  username: string;
-  password: string;
-  password_confirm: string;
-  email: string;
-}
+const registerSchema = object({
+  username: string().nonempty('Username is required'),
+  email: string().nonempty('Username is required').email("Email is invalid'"),
+  password: string()
+    .nonempty('Password is required')
+    .min(8, 'Password must be more than 8 characters')
+    .max(32, 'Password must be less than 32 characters'),
+  password_confirm: string()
+    .nonempty('Password Confirm is required')
+    .min(8, 'Password must be more than 8 characters')
+    .max(32, 'Password must be less than 32 characters'),
+})
+
+type RegisterInput = TypeOf<typeof registerSchema>;
+
+export const RegisterPage = () => {
+
+  const {
+    register,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+    handleSubmit,
+  } = useForm<RegisterInput>({
+    resolver: zodResolver(registerSchema),
+  });
 
 
-const RegisterForm = () => {
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+  }, [isSubmitSuccessful, reset]);
+
   const navigate = useNavigate()
-  const {register, handleSubmit} = useForm<IRegisterFormInput>();
-  const onSubmit: SubmitHandler<IRegisterFormInput> = async (data) => {
-    const response = await sendData(registerUrl, data);
+
+  const onSubmitHandler: SubmitHandler<RegisterInput> = async (values) => {
+    const {password, password_confirm, email, username} = values
+    const response = await sendData(registerUrl, {password, password_confirm, email, username});
     if (response.ok) {
       navigate("/login")
     }
-  }
-
+  };
   return (
-    <div className={"auth__form"}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-
-        <input placeholder={"Username..."} {...register("username")} /> <br/>
-
-        <input placeholder={"Email..."} {...register("email")} /> <br/>
-
-        <input placeholder={"Password..."} {...register("password")} /> <br/>
-
-        <input placeholder={"Password Confirm..."} {...register("password_confirm")} /> <br/>
-
-        <input type="submit"/>
-      </form>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline/>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{m: 5, bgcolor: 'secondary.main'}}>
+          <LockOutlinedIcon/>
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit(onSubmitHandler)} noValidate sx={{mt: 1}}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Email Address"
+            type={"email"}
+            autoComplete="email"
+            error={!!errors['email']}
+            helperText={errors['email'] ? errors['email'].message : ''}
+            {...register('email')}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Username"
+            error={!!errors['username']}
+            helperText={errors['username'] ? errors['username'].message : ''}
+            {...register('username')}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Password"
+            type="password"
+            error={!!errors['password']}
+            helperText={errors['password'] ? errors['password'].message : ''}
+            {...register('password')}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Password Confrim"
+            type="password"
+            error={!!errors['password_confirm']}
+            helperText={errors['password_confirm'] ? errors['password_confirm'].message : ''}
+            {...register('password_confirm')}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{mt: 3, mb: 2}}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link onClick={() => navigate("/login")}  variant="body2">
+                {"Already have an account? Sign In"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
-}
-
-
-export const RegisterPage = () => {
-  return (
-    <RegisterForm/>
-  )
 }
